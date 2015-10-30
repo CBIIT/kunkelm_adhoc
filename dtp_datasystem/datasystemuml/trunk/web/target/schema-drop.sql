08156693be8d549aa4e93ba5f835f47658590468
kunkelm <kunkelm@ncias-d1611-v.nci.nih.gov>
2015-10-30 17:49:58 +0000

    Renamed "comment" fields to include entity -> entityComment since "comment" is keyword in Oracle.   Reworked pom.xml to build Oracle version of schema for replication from PostGres to Oracle.

--- dtp_datasystem/datasystemuml/trunk/web/target/schema-drop.sql
+++ dtp_datasystem/datasystemuml/trunk/web/target/schema-drop.sql
@@ -1,170 +1,75 @@
 
-    alter table AD_HOC_CMPD 
-        drop constraint AD_HOC_CMPDIFKC;
+    drop table AD_HOC_CMPD cascade constraints;
 
-    alter table AD_HOC_CMPD 
-        drop constraint AD_HOC_CMPD_AD_HOC_CMPD_PARENC;
+    drop table AD_HOC_CMPD_FRAGMENT cascade constraints;
 
-    alter table AD_HOC_CMPD_FRAGMENT 
-        drop constraint AD_HOC_CMPD_FRAGMENT_AD_HOC_CH;
+    drop table AD_HOC_CMPD_FRAGMENT_P_CHEM cascade constraints;
 
-    alter table AD_HOC_CMPD_FRAGMENT 
-        drop constraint AD_HOC_CMPD_FRAGMENT_AD_HOC_CC;
+    drop table AD_HOC_CMPD_FRAGMENT_STRUCTURE cascade constraints;
 
-    alter table AD_HOC_CMPD_FRAGMENT 
-        drop constraint AD_HOC_CMPD_FRAGMENT_AD_HOC_CD;
+    drop table CMPD cascade constraints;
 
-    alter table CMPD_ALIAS 
-        drop constraint CMPD_ALIAS_CMPD_ALIAS_TYPE_FKC;
+    drop table CMPD_ALIAS cascade constraints;
 
-    alter table CMPD_ALIASES2NSC_CMPDS 
-        drop constraint CMPD_ALIAS_NSC_CMPDS_FKC;
+    drop table CMPD_ALIASES2NSC_CMPDS cascade constraints;
 
-    alter table CMPD_ALIASES2NSC_CMPDS 
-        drop constraint NSC_CMPD_CMPD_ALIASES_FKC;
+    drop table CMPD_ALIAS_TYPE cascade constraints;
 
-    alter table CMPD_FRAGMENT 
-        drop constraint CMPD_FRAGMENT_NSC_CMPD_FKC;
+    drop table CMPD_ANNOTATION cascade constraints;
 
-    alter table CMPD_FRAGMENT 
-        drop constraint CMPD_FRAGMENT_CMPD_KNOWN_SALTC;
+    drop table CMPD_BIO_ASSAY cascade constraints;
 
-    alter table CMPD_FRAGMENT 
-        drop constraint CMPD_FRAGMENT_CMPD_FRAGMENT_SC;
+    drop table CMPD_FRAGMENT cascade constraints;
 
-    alter table CMPD_FRAGMENT 
-        drop constraint CMPD_FRAGMENT_CMPD_FRAGMENT_PC;
+    drop table CMPD_FRAGMENT_P_CHEM cascade constraints;
 
-    alter table CMPD_LIST_MEMBER 
-        drop constraint CMPD_LIST_MEMBER_CMPD_LIST_FKC;
+    drop table CMPD_FRAGMENT_STRUCTURE cascade constraints;
 
-    alter table CMPD_LIST_MEMBER 
-        drop constraint CMPD_LIST_MEMBER_CMPD_FKC;
+    drop table CMPD_FRAGMENT_TYPE cascade constraints;
 
-    alter table CMPD_NAMED_SETS2NSC_CMPDS 
-        drop constraint CMPD_NAMED_SET_NSC_CMPDS_FKC;
+    drop table CMPD_INVENTORY cascade constraints;
 
-    alter table CMPD_NAMED_SETS2NSC_CMPDS 
-        drop constraint NSC_CMPD_CMPD_NAMED_SETS_FKC;
+    drop table CMPD_KNOWN_SALT cascade constraints;
 
-    alter table CMPD_PLATES2NSC_CMPDS 
-        drop constraint CMPD_PLATE_NSC_CMPDS_FKC;
+    drop table CMPD_LEGACY_CMPD cascade constraints;
 
-    alter table CMPD_PLATES2NSC_CMPDS 
-        drop constraint NSC_CMPD_CMPD_PLATES_FKC;
+    drop table CMPD_LIST cascade constraints;
 
-    alter table CMPD_PROJECTS2NSC_CMPDS 
-        drop constraint CMPD_PROJECT_NSC_CMPDS_FKC;
+    drop table CMPD_LIST_MEMBER cascade constraints;
 
-    alter table CMPD_PROJECTS2NSC_CMPDS 
-        drop constraint NSC_CMPD_CMPD_PROJECTS_FKC;
+    drop table CMPD_NAMED_SET cascade constraints;
 
-    alter table CMPD_PUB_CHEM_SIDS2NSC_CMPDS 
-        drop constraint CMPD_PUB_CHEM_SID_NSC_CMPDS_FC;
+    drop table CMPD_NAMED_SETS2NSC_CMPDS cascade constraints;
 
-    alter table CMPD_PUB_CHEM_SIDS2NSC_CMPDS 
-        drop constraint NSC_CMPD_CMPD_PUB_CHEM_SIDS_FC;
+    drop table CMPD_PLATE cascade constraints;
 
-    alter table CMPD_RELATED 
-        drop constraint CMPD_RELATED_NSC_CMPD_FKM;
+    drop table CMPD_PLATES2NSC_CMPDS cascade constraints;
 
-    alter table CMPD_RELATED 
-        drop constraint CMPD_RELATED_CMPD_RELATION_TYC;
+    drop table CMPD_PROJECT cascade constraints;
 
-    alter table CMPD_TARGETS2NSC_CMPDS 
-        drop constraint CMPD_TARGET_NSC_CMPDS_FKC;
+    drop table CMPD_PROJECTS2NSC_CMPDS cascade constraints;
 
-    alter table CMPD_TARGETS2NSC_CMPDS 
-        drop constraint NSC_CMPD_CMPD_TARGETS_FKC;
+    drop table CMPD_PUB_CHEM_SID cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPD_CMPD_LEGACY_CMPD_FKC;
+    drop table CMPD_PUB_CHEM_SIDS2NSC_CMPDS cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPD_CMPD_INVENTORY_FKC;
+    drop table CMPD_RELATED cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPDIFKC;
+    drop table CMPD_RELATION_TYPE cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPD_NSC_CMPD_TYPE_FKC;
+    drop table CMPD_TABLE cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPD_CMPD_PARENT_FRAGMENTC;
+    drop table CMPD_TARGET cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPD_CMPD_ANNOTATION_FKC;
+    drop table CMPD_TARGETS2NSC_CMPDS cascade constraints;
 
-    alter table NSC_CMPD 
-        drop constraint NSC_CMPD_CMPD_BIO_ASSAY_FKC;
+    drop table NSC_CMPD cascade constraints;
 
-    drop table AD_HOC_CMPD cascade;
+    drop table NSC_CMPD_TYPE cascade constraints;
 
-    drop table AD_HOC_CMPD_FRAGMENT cascade;
+    drop table RDKIT_MOL cascade constraints;
 
-    drop table AD_HOC_CMPD_FRAGMENT_P_CHEM cascade;
-
-    drop table AD_HOC_CMPD_FRAGMENT_STRUCTURE cascade;
-
-    drop table CMPD cascade;
-
-    drop table CMPD_ALIAS cascade;
-
-    drop table CMPD_ALIASES2NSC_CMPDS cascade;
-
-    drop table CMPD_ALIAS_TYPE cascade;
-
-    drop table CMPD_ANNOTATION cascade;
-
-    drop table CMPD_BIO_ASSAY cascade;
-
-    drop table CMPD_FRAGMENT cascade;
-
-    drop table CMPD_FRAGMENT_P_CHEM cascade;
-
-    drop table CMPD_FRAGMENT_STRUCTURE cascade;
-
-    drop table CMPD_INVENTORY cascade;
-
-    drop table CMPD_KNOWN_SALT cascade;
-
-    drop table CMPD_LEGACY_CMPD cascade;
-
-    drop table CMPD_LIST cascade;
-
-    drop table CMPD_LIST_MEMBER cascade;
-
-    drop table CMPD_NAMED_SET cascade;
-
-    drop table CMPD_NAMED_SETS2NSC_CMPDS cascade;
-
-    drop table CMPD_PLATE cascade;
-
-    drop table CMPD_PLATES2NSC_CMPDS cascade;
-
-    drop table CMPD_PROJECT cascade;
-
-    drop table CMPD_PROJECTS2NSC_CMPDS cascade;
-
-    drop table CMPD_PUB_CHEM_SID cascade;
-
-    drop table CMPD_PUB_CHEM_SIDS2NSC_CMPDS cascade;
-
-    drop table CMPD_RELATED cascade;
-
-    drop table CMPD_RELATION_TYPE cascade;
-
-    drop table CMPD_TABLE cascade;
-
-    drop table CMPD_TARGET cascade;
-
-    drop table CMPD_TARGETS2NSC_CMPDS cascade;
-
-    drop table NSC_CMPD cascade;
-
-    drop table NSC_CMPD_TYPE cascade;
-
-    drop table RDKIT_MOL cascade;
+    drop table TANIMOTO_SCORES cascade constraints;
 
     drop sequence AD_HOC_CMPD_FRAGMENT_P_CHE_SEQ;
 
@@ -187,6 +92,8 @@
     drop sequence CMPD_INVENTORY_SEQ;
 
     drop sequence CMPD_KNOWN_SALT_SEQ;
+
+    drop sequence CMPD_LEGACY_CMPD_SEQ;
 
     drop sequence CMPD_LIST_MEMBER_SEQ;
 
